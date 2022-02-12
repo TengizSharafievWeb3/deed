1874e40f8d1bde95fe38004c556a14be8197b6d0
Tengiz Sharafiev <btolfa@gmail.com>
2022-02-12 16:56:21 +0700

    Add deed program and anchor tests

--- programs/deed/src/lib.rs
+++ programs/deed/src/lib.rs
@@ -1,14 +1,72 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{
+    clock::Clock,
+    system_instruction,
+};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod deed {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, lawyer: Pubkey, beneficiary: Pubkey, from_now: u64, amount: u64) -> ProgramResult {
+        {
+            let deed = &mut ctx.accounts.deed;
+            deed.lawyer = lawyer;
+            deed.beneficiary = beneficiary;
+            deed.earliest = (Clock::get()?.unix_timestamp).saturating_add(from_now as i64);
+        }
+
+        let deed_lamport = ctx.accounts.deed.to_account_info().lamports();
+
+        if deed_lamport < amount
+        {
+            let ix = system_instruction::transfer(&ctx.accounts.user.key(), &ctx.accounts.deed.key(), amount - deed_lamport);
+            anchor_lang::solana_program::program::invoke(
+                &ix, &[
+                    ctx.accounts.user.to_account_info(),
+                    ctx.accounts.deed.to_account_info(),
+            ])?;
+        }
+
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>) -> ProgramResult {
+        require!(Clock::get()?.unix_timestamp >= ctx.accounts.deed.earliest, DeedError::TooEarly);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user)]
+    pub deed: Account<'info, Deed>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut, close = beneficiary, constraint = deed.lawyer == lawyer.key() && deed.beneficiary == beneficiary.key())]
+    pub deed: Account<'info, Deed>,
+    pub lawyer: Signer<'info>,
+    #[account(mut)]
+    pub beneficiary: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Deed {
+    pub lawyer : Pubkey,
+    pub beneficiary: Pubkey,
+    pub earliest: i64,
+}
+
+#[error]
+pub enum DeedError {
+    #[msg("too early")]
+    TooEarly
+}
--- tests/deed.ts
+++ tests/deed.ts
@@ -1,6 +1,12 @@
 import * as anchor from '@project-serum/anchor';
+import { web3 } from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { Deed } from '../target/types/deed';
+import { expect } from 'chai'
+
+import * as chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 
 describe('deed', () => {
 
@@ -8,10 +14,102 @@ describe('deed', () => {
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Deed as Program<Deed>;
+  const provider = anchor.getProvider();
+  const wallet = provider.wallet;
+  const lawyer = web3.Keypair.generate();
+  const beneficiary = web3.Keypair.generate();
+  /*
+  const zeroAccountRentExempt = await provider.connection.getMinimumBalanceForRentExemption(0);
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  provider.send(new web3.Transaction().add(
+    web3.SystemProgram.createAccount({
+      fromPubkey: wallet.publicKey,
+      lamports: zeroAccountRentExempt,
+      newAccountPubkey: beneficiary.publicKey,
+      programId: web3.SystemProgram.programId,
+      space: 0,
+    })
+  ));
+  */
+
+  it('Init', async () => {
+    const deed = web3.Keypair.generate();
+
+    await program.methods
+      .initialize(lawyer.publicKey, beneficiary.publicKey, new anchor.BN(1), new anchor.BN(web3.LAMPORTS_PER_SOL))
+      .accounts({deed: deed.publicKey})
+      .signers([deed])
+      .rpc();
+    
+    const deedAccount = await program.account.deed.fetch(deed.publicKey);
+    expect(deedAccount.lawyer).to.be.deep.equal(lawyer.publicKey);
+    expect(deedAccount.beneficiary).to.be.deep.equal(beneficiary.publicKey);
+    
+    const deedBalance = await provider.connection.getBalance(deed.publicKey);
+    expect(deedBalance).to.be.equal(web3.LAMPORTS_PER_SOL);
+  });
+
+  it('Withdraw', async() => {
+    // Create non-empty beneficiary account
+    const zeroAccountRentExempt = await provider.connection.getMinimumBalanceForRentExemption(0);
+    provider.send(new web3.Transaction().add(
+      web3.SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        lamports: zeroAccountRentExempt,
+        newAccountPubkey: beneficiary.publicKey,
+        programId: web3.SystemProgram.programId,
+        space: 0,
+      })
+    ));
+    const beneficiaryBalanceBefore = await provider.connection.getBalance(beneficiary.publicKey);
+
+    const deed = web3.Keypair.generate();
+    await program.methods
+      .initialize(lawyer.publicKey, beneficiary.publicKey, new anchor.BN(1), new anchor.BN(web3.LAMPORTS_PER_SOL))
+      .accounts({deed: deed.publicKey})
+      .signers([deed])
+      .rpc();
+    const deedBalance = await provider.connection.getBalance(deed.publicKey);
+    expect(deedBalance).to.be.equal(web3.LAMPORTS_PER_SOL);
+    
+    // Sleep for 2 sec
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    await program.methods
+      .withdraw()
+      .accounts({
+        deed: deed.publicKey,
+        lawyer: lawyer.publicKey,
+        beneficiary: beneficiary.publicKey,
+      })
+      .signers([lawyer])
+      .rpc();
+
+    const deedAccount = await program.account.deed.fetchNullable(deed.publicKey);
+    expect(deedAccount).to.be.null;
+
+    const beneficiaryBalanceAfter = await provider.connection.getBalance(beneficiary.publicKey);
+    expect(beneficiaryBalanceAfter - beneficiaryBalanceBefore).to.be.equal(web3.LAMPORTS_PER_SOL);
+  });
+
+  it('Should NOT withdraw early', async() => {
+    const deed = web3.Keypair.generate();
+    await program.methods
+      .initialize(lawyer.publicKey, beneficiary.publicKey, new anchor.BN(60), new anchor.BN(web3.LAMPORTS_PER_SOL))
+      .accounts({deed: deed.publicKey})
+      .signers([deed])
+      .rpc();
+    const deedBalance = await provider.connection.getBalance(deed.publicKey);
+    expect(deedBalance).to.be.equal(web3.LAMPORTS_PER_SOL);
+
+    await expect(program.methods
+      .withdraw()
+      .accounts({
+        deed: deed.publicKey,
+        lawyer: lawyer.publicKey,
+        beneficiary: beneficiary.publicKey,
+      })
+      .signers([lawyer])
+      .rpc()).to.be.rejectedWith(/too early/);
   });
 });
